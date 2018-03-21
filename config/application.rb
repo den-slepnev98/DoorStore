3634f23ff9a85b57615b10d242251d061e7a3819
Den Slepnev <den.slepnev1998@gmail.com>
2018-03-21 19:11:20 +0200

    fixed Fonts

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,7 @@ module DoorStore
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.initialize_on_precompile = false
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
